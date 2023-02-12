51511234e24a6a2a0a3747a131d60afbe120a4c8
druiz912 <druiz264@gmail.com>
2023-02-12 23:25:11 +0100

    refactor entities

--- back/src/main/java/com/druiz/fullstack/back/domain/Tablero.java
+++ back/src/main/java/com/druiz/fullstack/back/domain/Tablero.java
@@ -1,45 +1,75 @@
 package com.druiz.fullstack.back.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-import javax.validation.constraints.NotNull;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "boards")
 @Data
-@Builder
 @NoArgsConstructor
-public class Board {
+/**
+ * Representa un tablero de juego de 4 en raya.
+ */
+public class Tablero {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    // Número de filas
-    @NotNull
-    private int numRows;
-    // Número de columnas
-    @NotNull
-    private int numColumns;
-    @NotNull
-    private int idHostPlayer;
-    @NotNull
-    private int idGuestPlayer;
-    @NotNull
-    private int[][] matriz;
+    private int filas;
+    private int columnas;
+    private List<Ficha> fichas;
 
-    private GameStatus status;
+    /**
+     * Crea un nuevo tablero de juego de 4 en raya.
+     *
+     * @param filas el número de filas del tablero
+     * @param columnas el número de columnas del tablero
+     */
+    public Tablero(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.fichas = new ArrayList<>();
+    }
 
+    /**
+     * Devuelve el número de filas del tablero.
+     *
+     * @return el número de filas del tablero
+     */
+    public int getFilas() {
+        return filas;
+    }
 
-    public Board(int id, int numRows, int numColumns, int idHostPlayer, int idGuestPlayer, @NotNull int[][] matriz, GameStatus status) {
-        this.id = id;
-        this.numRows = numRows;
-        this.numColumns = numColumns;
-        this.idHostPlayer = idHostPlayer;
-        this.idGuestPlayer = idGuestPlayer;
-        this.matriz = matriz;
-        this.status = status;
+    /**
+     * Devuelve el número de columnas del tablero.
+     *
+     * @return el número de columnas del tablero
+     */
+    public int getColumnas() {
+        return columnas;
+    }
+
+    /**
+     * Devuelve la lista de fichas colocadas en el tablero.
+     *
+     * @return la lista de fichas colocadas en el tablero
+     */
+    public List<Ficha> getFichas() {
+        return fichas;
+    }
+
+    /**
+     * Agrega una nueva ficha al tablero.
+     *
+     * @param ficha la ficha a agregar
+     */
+    public void agregarFicha(Ficha ficha) {
+        fichas.add(ficha);
     }
 }
