077b8d561faf522d217e41bbc21f138280036b98
druiz912 <druiz264@gmail.com>
2023-02-13 15:45:39 +0100

    new entity jugador

--- back/src/main/java/com/druiz/fullstack/back/domain/Jugador.java
+++ back/src/main/java/com/druiz/fullstack/back/domain/Jugador.java
@@ -1,19 +1,20 @@
 package com.druiz.fullstack.back.domain;
 
-import com.druiz.fullstack.back.infrastructure.controller.dto.input.PlayerInputDto;
+import com.druiz.fullstack.back.infrastructure.controller.dto.input.JugadorInputDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Reference;
 import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.NotNull;
 
-@Table(name = "players")
+@Table(name = "jugadores")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Player {
+public class Jugador {
 
     @Id
     private Integer id;
@@ -21,8 +22,25 @@ public class Player {
     @NotNull
     private String userPlayer;
 
+    /**
+     * Relación 1:1 con la entidad User.
+     * La entidad Jugador tiene una referencia a un objeto de la entidad User.
+     * @Reference Tiene dos campos:
+     *  1. El campo value especifica la entidad a la que se refiere la relación.
+     *  2. El campo to especifica la clase a la que va en la entidad actual (Jugador), se usará para la relación
+     */
+    @Reference(
+            value = User.class,
+            to = Jugador.class
+    )
+    private User user;
 
-    public Player(PlayerInputDto playerInputDto) {
-        userPlayer =  playerInputDto.getUserPlayer();
+    public Jugador(int id, String userPlayer){
+        this.id = id;
+        this.userPlayer = userPlayer;
+    }
+
+    public Jugador(JugadorInputDto dto) {
+        this.userPlayer =  dto.getUserPlayer();
     }
 }
