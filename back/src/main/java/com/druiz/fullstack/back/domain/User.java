4bb7eab8b74b25290561cbf1d057130ad8315d0f
druiz912 <druiz264@gmail.com>
2023-02-14 20:27:23 +0100

    javadoc and implements UserDetails

--- back/src/main/java/com/druiz/fullstack/back/domain/User.java
+++ back/src/main/java/com/druiz/fullstack/back/domain/User.java
@@ -1,5 +1,6 @@
 package com.druiz.fullstack.back.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,52 +16,126 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Clase que representa un usuario en la aplicación.
+ * Implementa la interfaz UserDetails de Spring Security,
+ * lo que permite su uso en la autenticación y autorización de usuarios.
+ */
 @Table(name = "users")
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements UserDetails {
-    @Id
-    private String id;
-    private String username;
-    private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
+    /**
+      * Identificador único del usuario.
+      */
+     @Id
+     private String id;
+
+     /**
+      * Nombre de usuario.
+      */
+     private String username;
+
+     /**
+      * Contraseña del usuario.
+      */
+     private String password;
+
+     /**
+      * Lista de roles asociados al usuario.
+      */
+     @ManyToMany(fetch = FetchType.EAGER)
      private List<Role> roles;
 
-     // constructores, getters y setters
+     /**
+      * Lista de autoridades/granted authorities asignadas al usuario.
+      */
+     private List<GrantedAuthority> authorities;
 
-    // TODO: Cambiar esta lógica
-     @Override
-     public Collection<? extends GrantedAuthority> getAuthorities() {
-         return roles.stream()
-                 .map(role -> new SimpleGrantedAuthority(role.getName()))
-                 .toList();
-     }
 
+    /**
+       * Constructor de la clase User.
+       * @param username el nombre de usuario del usuario.
+       * @param password la contraseña del usuario.
+       * @param authorities la lista de autoridades/granted authorities asignadas al usuario.
+       */
+      public User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+          this.username = username;
+          this.password = password;
+          this.authorities = new ArrayList<>(authorities);
+      }
+
+
+    /**
+     * Retorna la lista de roles del usuario,
+     * la cual es usada por Spring Security para realizar la autorización de acciones en la aplicación.
+     *
+     * @return la lista de roles del usuario.
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    /**
+     * Retorna el password del usuario,
+     * que es utilizado para la autenticación del mismo en la aplicación.
+     *
+     * @return el password del usuario.
+     */
     @Override
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * Retorna el nombre de usuario del usuario,
+     * que es utilizado para la autenticación del mismo en la aplicación.
+     *
+     * @return el nombre de usuario del usuario.
+     */
     @Override
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * Retorna un booleano que indica si la cuenta del usuario ha expirado o no.
+     *
+     * @return un booleano que indica si la cuenta del usuario ha expirado o no.
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    /**
+     * Retorna un booleano que indica si la cuenta del usuario ha sido bloqueada o no.
+     *
+     * @return un booleano que indica si la cuenta del usuario ha sido bloqueada o no.
+     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    /**
+     * Retorna un booleano que indica si las credenciales del usuario han expirado o no.
+     *
+     * @return un booleano que indica si las credenciales del usuario han expirado o no.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    /**
+     * Retorna un booleano que indica si el usuario está habilitado o no.
+     *
+     * @return un booleano que indica si el usuario está habilitado o no.
+     */
     @Override
     public boolean isEnabled() {
         return true;
