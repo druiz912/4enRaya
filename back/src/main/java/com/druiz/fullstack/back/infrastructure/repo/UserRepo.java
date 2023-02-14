267a9769d2ca7031e654aba76667dd2c0f22a51c
druiz912 <druiz264@gmail.com>
2023-02-14 02:41:33 +0100

    add new method in repo

--- back/src/main/java/com/druiz/fullstack/back/infrastructure/repo/UserRepo.java
+++ back/src/main/java/com/druiz/fullstack/back/infrastructure/repo/UserRepo.java
@@ -2,7 +2,8 @@ package com.druiz.fullstack.back.infrastructure.repo;
 
 import com.druiz.fullstack.back.domain.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepo extends ReactiveCrudRepository<User, Integer> {
-    User findByUsername(String username);
+    Mono<User> findByUsername(String username);
 }
