e20ea0950b2f98a6ce296c403be01b97b712fce6
druiz912 <druiz264@gmail.com>
2023-02-14 02:40:25 +0100

    update dto for Jugador

--- back/src/main/java/com/druiz/fullstack/back/infrastructure/controller/dto/input/JugadorInputDto.java
+++ back/src/main/java/com/druiz/fullstack/back/infrastructure/controller/dto/input/JugadorInputDto.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 /**
  * Data Transfer Object para crear una entidad Jugador.
  * @Data (Lombok) y genera automáticamente los métodos get, set, equals, hashCode, etc.
@@ -14,6 +17,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JugadorInputDto {
+
+    @NotNull
     private String userPlayer;
+
+    @Min(value = 1)
     private Integer idUser;
 }
